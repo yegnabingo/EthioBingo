5634bd1b5677b7530a5dd10e2068caa961d34893
yegnabingo <yegnabingo@gmail.com>
2026-07-05 02:20:41 +0300

    Delete app/admin.py

--- app/admin.py
+++
@@ -1,8 +0,0 @@
-# app/admin.py
-# Admin utilities placeholder
-
-# TODO: add admin routes, commands, or scripts
-
-def create_admin(username: str):
-    # placeholder
-    return {"username": username, "role": "admin"}
