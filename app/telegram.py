cda31e433a30edc9ffcb7ba63624f8ef0e429e9a
yegnabingo <yegnabingo@gmail.com>
2026-08-04 01:43:18 +0300

    Change welcome image URL extension to .jpeg

--- app/telegram.py
+++ app/telegram.py
@@ -20,7 +20,7 @@ BACKEND_URL = SERVER_URL
 MINI_APP_URL = SERVER_URL
 
 # 🖼️ የማቀባበያ ምስል ሊንክ
-WELCOME_IMAGE_URL = f"{SERVER_URL}/static/images/welcome.png"
+WELCOME_IMAGE_URL = f"{SERVER_URL}/static/images/welcome.png.jpeg"
 
 bot = TeleBot(BOT_TOKEN)
 
