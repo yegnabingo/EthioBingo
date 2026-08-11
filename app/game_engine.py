8e61075a9f7401362a9e7b4f2bf18a4bdacb0aa3
yegnabingo <yegnabingo@gmail.com>
2026-08-11 20:01:28 +0300

    Update game_engine.py

--- app/game_engine.py
+++ app/game_engine.py
@@ -56,10 +56,15 @@ class GameEngine:
         now = datetime.now(timezone.utc)
         hour = (now.hour + 3) % 24
 
-        if 6 <= hour < 18:
-            return random.randint(50, 100)
-        elif 18 <= hour <= 23:
+        # ከጠዋቱ 12:00 እስከ ቀኑ 7:00 (ከ6:00 እስከ 12:59)
+        if 6 <= hour < 13:
             return random.randint(40, 80)
+
+        # ከቀኑ 7:00 እስከ ሌሊቱ 6:00 (ከ13:00 እስከ 23:59)
+        elif 13 <= hour <= 23:
+            return random.randint(100, 150)
+
+        # ከሌሊቱ 6:00 እስከ ጠዋቱ 12:00 (ከ0:00 እስከ 5:59)
         else:
             return random.randint(30, 60)
 
